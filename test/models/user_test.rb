2104a595027d3dbdc06ad128c9320fac5e07c53e
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-05-29 13:44:42 +0200

    test for valid email format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,14 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
   test "should be valid" do
     assert @user.valid?
   end
